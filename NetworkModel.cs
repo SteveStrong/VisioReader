31b7a9f477cf7c5752ef263cafea98501405d617
Steve <stephen.r.strong@saic.com>
2025-05-31 17:56:27 -0400

    Add initial implementation of NodeShape, EdgeShape, and NetworkModel classes

---
+++ NetworkModel.cs
@@ -0,0 +1,34 @@
+
+public class NodeShape
+{
+    public string? ID { get; set; }
+    public string? Text { get; set; }
+    public double? PinX { get; set; }
+    public double? PinY { get; set; }
+}
+
+public class EdgeShape
+{
+    public string? ID { get; set; }
+    public string? Text { get; set; }
+    public double? PinX { get; set; }
+    public double? PinY { get; set; }
+    public string? FromNodeID { get; set; }
+    public string? ToNodeID { get; set; }
+}
+
+public class NetworkModel
+{
+    public List<NodeShape> Nodes { get; set; } = new List<NodeShape>();
+    public List<EdgeShape> Edges { get; set; } = new List<EdgeShape>();
+
+    public void AddNode(NodeShape node)
+    {
+        Nodes.Add(node);
+    }
+
+    public void AddEdge(EdgeShape edge)
+    {
+        Edges.Add(edge);
+    }
+}
